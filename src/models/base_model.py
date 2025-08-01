39c28e7dda24268b77585cc82b0d3af3d4b9b168
lhiradi <hiradjalilian@gmail.com>
2025-08-02 01:30:10 +0330

    updated model and compose file

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -9,7 +9,7 @@ class BaseModel(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
-    password_hash = db.Column(db.String)
+    password_hash = db.Column(db.String(255))
     date_of_birth = db.Column(db.Date)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
